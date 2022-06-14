b9d0f0693a87f84f712953d72831d0f1c0c643b5
Ignacio Fuentes <ignaciofuentes@outlook.com>
2022-06-14 16:28:41 -0400

    updates

---
+++ Blazorons3/Models/GitHubBranch.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Blazorons3.Models
+{
+    public class GitHubBranch
+    {
+        [JsonPropertyName("name")]
+        public string? Name { get; set; }
+    }
+}
+
